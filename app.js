9fc5b541b2545f3a986ecb04e308848da79a2be2
knud21kea <grah0070@stud.kea.dk>
2023-10-10 01:30:48 +0200

    onrender

--- app.js
+++ app.js
@@ -106,5 +106,6 @@ app.post('/api/login', ((req, res) => {
 
 // Server --------------------------------------------------------------------
 
-const PORT = 8080;
+// const PORT = 8080;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => console.log('Server is running on port', PORT));
